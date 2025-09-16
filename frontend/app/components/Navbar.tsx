3bf1574da8e09abb204a9d0f733a5e760ab85db5
Jhonathas Souza <jhonathassouza8@gmail.com>
2025-09-16 13:18:58 -0300

    feat: create responsive Navbar with logo and navigation link

---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+import Logo from "../../public/logo.svg"
+import Link from "next/link";
+
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-center md:justify-start items-center 
+    mt-[50px] md:mt-[50px] ml-0 md:ml-[55px] w-full'>
+      <div>
+        <Link href="/" className="flex items-center gap-[8px]">
+          <Image priority src={Logo} height={74} width={74} alt="Logo" />
+          <h1 className='text-4xl font-bold'>SoftPet</h1>
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
